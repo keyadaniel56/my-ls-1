dbc8116a31885c428bb81f9fab3d7b0491c9f2d7
keyadaniel56 <keyadaniel56@gmail.com>
2026-03-30 11:05:50 +0300

    implemented the sorting functionalities

--- internal/ls/sort.go
+++ internal/ls/sort.go
@@ -1,1 +1,27 @@
 package ls
+
+import (
+	"sort"
+
+	"my-ls-1/internal/models"
+)
+
+func Sort(entries []models.FileEntry, flags models.Flags) []models.FileEntry {
+	if flags.SortTime {
+		sort.Slice(entries, func(i, j int) bool {
+			return entries[i].Info.ModTime().After(entries[j].Info.ModTime())
+		})
+	} else {
+		sort.Slice(entries, func(i, j int) bool {
+			return entries[i].Name < entries[j].Name
+		})
+	}
+
+	if flags.Reverse {
+		for i, j := 0, len(entries)-1; i < j; i, j = i+1, j-1 {
+			entries[i], entries[j] = entries[j], entries[i]
+		}
+	}
+
+	return entries
+}
