d7eb66bdad882308e593af71d6f5b12a325c9c26
keyadaniel56 <keyadaniel56@gmail.com>
2026-03-30 11:13:15 +0300

    implemented the printlong func which prints the long format similar to ls -l

--- internal/ls/long.go
+++ internal/ls/long.go
@@ -1,1 +1,36 @@
 package ls
+
+import (
+	"fmt"
+	"os/user"
+	"strconv"
+	"syscall"
+	"time"
+
+	"my-ls-1/internal/models"
+)
+
+func PrintLong(entries []models.FileEntry) {
+	for _, e := range entries {
+		info := e.Info
+
+		stat := info.Sys().(*syscall.Stat_t)
+
+		u, _ := user.LookupId(strconv.Itoa(int(stat.Uid)))
+		g, _ := user.LookupGroupId(strconv.Itoa(int(stat.Gid)))
+
+		fmt.Printf("%s %d %s %s %d %s %s\n",
+			info.Mode().String(),
+			stat.Nlink,
+			u.Username,
+			g.Name,
+			info.Size(),
+			formatTime(info.ModTime()),
+			e.Name,
+		)
+	}
+}
+
+func formatTime(t time.Time) string {
+	return t.Format("Jan _2 15:04")
+}
