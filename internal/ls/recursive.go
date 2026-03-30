b386e913bd67d3a7f8201201e27744fc601fc537
keyadaniel56 <keyadaniel56@gmail.com>
2026-03-30 11:19:23 +0300

    implemented the printsimple and the recusive function

--- internal/ls/recursive.go
+++ internal/ls/recursive.go
@@ -1,1 +1,44 @@
 package ls
+
+import (
+	"fmt"
+	"os"
+
+	"my-ls-1/internal/models"
+)
+
+func Recursive(base string, entries []models.FileEntry, flags models.Flags) {
+	for _, e := range entries {
+		if e.IsDir && e.Name != "." && e.Name != ".." {
+			fmt.Printf("%s:\n", e.Path)
+
+			files, err := os.ReadDir(e.Path)
+			if err != nil {
+				continue
+			}
+
+			var newEntries []models.FileEntry
+
+			for _, f := range files {
+				info, _ := f.Info()
+				newEntries = append(newEntries, models.FileEntry{
+					Name:  f.Name(),
+					Path:  e.Path + "/" + f.Name(),
+					Info:  info,
+					IsDir: f.IsDir(),
+				})
+			}
+
+			newEntries = Sort(newEntries, flags)
+
+			if flags.Long {
+				PrintLong(newEntries)
+			} else {
+				PrintSimple(newEntries)
+			}
+
+			fmt.Println()
+			Recursive(e.Path, newEntries, flags)
+		}
+	}
+}
--- internal/ls/utils.go
+++ internal/ls/utils.go
@@ -1,1 +1,11 @@
 package ls
+
+import "my-ls-1/internal/models"
+
+
+func PrintSimple(entries []models.FileEntry){
+	for _,e:=range entries{
+		print(e.Name +" ")
+	}
+	println()
+}
